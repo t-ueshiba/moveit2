9341d4039f5b6df6d285a00f595d9c6d96ecd42e
t-ueshiba <t.ueshiba@aist.go.jp>
2026-06-27 13:54:12 +0900

    Take link_offset into consideration when computing cartesian path

--- moveit_ros/move_group/src/default_capabilities/cartesian_path_service_capability.cpp
+++ moveit_ros/move_group/src/default_capabilities/cartesian_path_service_capability.cpp
@@ -193,7 +193,8 @@ bool MoveGroupCartesianPathService::computeService(
           std::vector<moveit::core::RobotStatePtr> traj;
           res->fraction = moveit::core::CartesianInterpolator::computeCartesianPath(
               &start_state, jmg, traj, link_model, waypoints, global_frame,
-              moveit::core::MaxEEFStep(req->max_step), moveit::core::CartesianPrecision{}, constraint_fn);
+              moveit::core::MaxEEFStep(req->max_step), moveit::core::CartesianPrecision{}, constraint_fn, kinematics::KinematicsQueryOptions(),
+              start_state.getGlobalLinkTransform(link_model).inverse() * frame_pose);
           moveit::core::robotStateToRobotStateMsg(start_state, res->start_state);
 
           robot_trajectory::RobotTrajectory rt(context_->planning_scene_monitor_->getRobotModel(), req->group_name);
